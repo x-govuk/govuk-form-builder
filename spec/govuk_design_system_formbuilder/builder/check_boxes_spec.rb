520eddac83d25884ec09291df765e16f2f083609
Peter Yates <peter.yates@graphia.co.uk>
2019-08-02 15:53:50 +0100

    Replace checkbox block spec with shared group

--- spec/govuk_design_system_formbuilder/builder/check_boxes_spec.rb
+++ spec/govuk_design_system_formbuilder/builder/check_boxes_spec.rb
@@ -31,28 +31,7 @@ describe GOVUKDesignSystemFormBuilder::FormBuilder do
       let(:error_identifier) { 'person-projects-error' }
     end
 
-    context 'when passed a block' do
-      let(:block_h1) { 'The quick brown fox' }
-      let(:block_h2) { 'Jumped over the' }
-      let(:block_p) { 'Lazy dog.' }
-      subject do
-        builder.send(*args) do
-          builder.safe_join(
-            [
-              builder.tag.h1(block_h1),
-              builder.tag.h2(block_h2),
-              builder.tag.p(block_p)
-            ]
-          )
-        end
-      end
-
-      specify 'should include block content' do
-        expect(subject).to have_tag('h1', text: block_h1)
-        expect(subject).to have_tag('h2', text: block_h2)
-        expect(subject).to have_tag('p', text: block_p)
-      end
-    end
+    it_behaves_like 'a field that accepts arbitrary blocks of HTML'
 
     describe 'check boxes' do
       specify 'output should contain the correct number of check boxes' do
