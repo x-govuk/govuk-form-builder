d0cf9a61fb7f76c92d55c7bb096d01998bdff0c1
Peter Yates <peter.yates@graphia.co.uk>
2019-08-14 15:56:41 +0100

    Switch to using Slim::Template over eval 😅

--- guide/lib/examples/text_input.rb
+++ guide/lib/examples/text_input.rb
@@ -1,26 +1,26 @@
 module Examples
   module TextInput
     def text_field
-      "f.govuk_text_field :name"
+      "= f.govuk_text_field :name"
     end
 
     def text_field_with_label
       <<~SNIPPET
-        f.govuk_text_field :first_name,
+        = f.govuk_text_field :first_name,
           label: { text: 'First name' }
       SNIPPET
     end
 
     def text_field_with_hint
       <<~SNIPPET
-        f.govuk_text_field :last_name,
+        = f.govuk_text_field :last_name,
           hint_text: 'You can find it on your birth certificate'
       SNIPPET
     end
 
     def text_field_with_attributes
       <<~SNIPPET
-        f.govuk_text_field :job_title,
+        = f.govuk_text_field :job_title,
           placeholder: "Nuclear safety engineer",
           autocomplete: "organization-title"
       SNIPPET
@@ -28,7 +28,7 @@ module Examples
 
     def text_field_with_custom_width
       <<~SNIPPET
-        f.govuk_text_field :postcode, width: 'one-third'
+        = f.govuk_text_field :postcode, width: 'one-third'
       SNIPPET
     end
   end
