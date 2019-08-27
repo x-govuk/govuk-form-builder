136bcd0aedd926b5322b1621d390f41d13755360
Peter Yates <peter.yates@graphia.co.uk>
2019-08-27 13:27:14 +0100

    Add error handling page and ensure errors display

--- guide/lib/examples/error_handling.rb
+++ guide/lib/examples/error_handling.rb
@@ -2,10 +2,9 @@ module Examples
   module ErrorHandling
     def text_field_with_errors
       object.valid?
-
       <<~SNIPPET
-        = f.govuk_text_field :favourite_shade_of_red,
-          label: { text: 'What is your favourite shade of red?', tag: 'h3', size: 'm' }
+        = f.govuk_text_field :reference_number,
+          label: { text: 'What is your reference number?' }
       SNIPPET
     end
   end
--- guide/lib/setup/form_builder_objects.rb
+++ guide/lib/setup/form_builder_objects.rb
@@ -1,12 +1,24 @@
 module Setup
   module FormBuilderObjects
-    def builder
+
+    def builder(errors = false)
+      errors ? builder_with_errors : builder_without_errors
+    end
+
+    def builder_without_errors
       GOVUKDesignSystemFormBuilder::FormBuilder.new(:person, object, helper, {})
     end
-    alias_method :f, :builder
+
+    def builder_with_errors
+      GOVUKDesignSystemFormBuilder::FormBuilder.new(:person, object_with_errors, helper, {})
+    end
 
     def object
       Person.new
+    end
+
+    def object_with_errors
+      Person.new.tap { |p| p.valid? }
     end
 
     def helper
