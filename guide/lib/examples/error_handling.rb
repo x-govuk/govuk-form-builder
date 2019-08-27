6a167a5c37d086962adbcc64fa42d32bedb48ff8
Peter Yates <peter.yates@graphia.co.uk>
2019-08-27 18:16:44 +0100

    Add more detail to the error handling page

--- guide/lib/examples/error_handling.rb
+++ guide/lib/examples/error_handling.rb
@@ -3,7 +3,12 @@ module Examples
     def text_field_with_errors
       object.valid?
       <<~SNIPPET
+        = f.govuk_error_summary
+
+        h2.govuk-heading-m Register your interest in becoming a teacher
+
         = f.govuk_text_field :reference_number,
+          width: 10,
           label: { text: 'What is your reference number?' }
       SNIPPET
     end
