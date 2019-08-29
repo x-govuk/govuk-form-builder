cfaad315ec9f4b7a3e9a29df13135648305614a8
Peter Yates <peter.yates@graphia.co.uk>
2019-08-29 15:26:15 +0100

    Add first draft of injecting content page

---
+++ guide/lib/examples/injecting_content.rb
@@ -0,0 +1,23 @@
+module Examples
+  module InjectingContent
+    def text_area_with_injected_content
+      <<~SNIPPET
+        = f.govuk_collection_radio_buttons :department_id,
+          departments,
+          :id,
+          :name,
+          legend: { text: "Which department do you want to nominate for the 'Best Department' award?" },
+          hint_text: "The department will not be notified they've been nominated for an award until the annual awards evening." do
+
+            .govuk-warning-text
+              span.govuk-warning-text__icon aria-hidden=true !
+              strong.govuk-warning-text__text
+                span.govuk-warning-text__assistive
+                  Warning
+                ' Unlike in previous years, nominations are not
+                  anonymous, the list of voters will be published after the
+                  prizes have been awarded.
+      SNIPPET
+    end
+  end
+end
