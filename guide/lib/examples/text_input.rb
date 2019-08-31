658286cae85a077d1f0eedd3c5ebb72cf4cbaa9e
Peter Yates <peter.yates@graphia.co.uk>
2019-08-31 11:15:14 +0100

    Align widths to make the example clearer

--- guide/lib/examples/text_input.rb
+++ guide/lib/examples/text_input.rb
@@ -21,21 +21,21 @@ module Examples
         h3.govuk-heading-s
           ' Fractional widths
 
-        = f.govuk_text_field :full, width: 'full'
+        = f.govuk_text_field :full,           width: 'full'
         = f.govuk_text_field :three_quarters, width: 'three-quarters'
-        = f.govuk_text_field :one_half, width: 'one-half'
-        = f.govuk_text_field :one_third, width: 'one-third'
-        = f.govuk_text_field :one_quarter, width: 'one-quarter'
+        = f.govuk_text_field :one_half,       width: 'one-half'
+        = f.govuk_text_field :one_third,      width: 'one-third'
+        = f.govuk_text_field :one_quarter,    width: 'one-quarter'
 
         h3.govuk-heading-s
           ' Absolulte (character) widths
 
         = f.govuk_text_field :twenty, width: 20
-        = f.govuk_text_field :ten, width: 10
-        = f.govuk_text_field :five, width: 5
-        = f.govuk_text_field :four, width: 4
-        = f.govuk_text_field :three, width: 3
-        = f.govuk_text_field :two, width: 2
+        = f.govuk_text_field :ten,    width: 10
+        = f.govuk_text_field :five,   width: 5
+        = f.govuk_text_field :four,   width: 4
+        = f.govuk_text_field :three,  width: 3
+        = f.govuk_text_field :two,    width: 2
       SNIPPET
     end
   end
