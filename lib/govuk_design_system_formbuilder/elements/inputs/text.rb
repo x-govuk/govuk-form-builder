a3dc404792842d10aae3781868a9f9ebe21e13e9
Peter Yates <peter.yates@graphia.co.uk>
2019-09-26 11:49:59 +0100

    Restructure text fields and separate to classes
    Instead of passing an attribtue_type into the initialize method make the
    various input types inherit from Inputs::Base and have each subclass
    define the builder method. This is a cleaner approach, albeit slightly
    more-verbose

---
+++ lib/govuk_design_system_formbuilder/elements/inputs/text.rb
@@ -0,0 +1,11 @@
+module GOVUKDesignSystemFormBuilder
+  module Elements
+    module Inputs
+      class Text < Base
+        def builder_method
+          :text_field
+        end
+      end
+    end
+  end
+end
