7bfda90db1479250be2b933ce7273e753a5703fe
Peter Yates <peter.yates@graphia.co.uk>
2019-09-28 10:40:03 +0100

    Standardise label and legend sizes on errors form

--- guide/lib/examples/error_handling.rb
+++ guide/lib/examples/error_handling.rb
@@ -6,27 +6,28 @@ module Examples
       <<~SNIPPET
         = f.govuk_error_summary
 
-        h2.govuk-heading-m
+        h2.govuk-heading-l
           | Register your interest in becoming a teacher
 
         = f.govuk_text_field :welcome_pack_reference_number,
           width: 10,
-          label: { text: 'What is your reference number?' }
+          label: { text: 'What is your reference number?', size: 's' }
 
         = f.govuk_date_field :welcome_pack_received_on,
-          legend: { text: 'When did you receive your welcome pack?' }
+          legend: { text: 'When did you receive your welcome pack?', size: 's' }
 
         = f.govuk_collection_select :department_id,
           departments,
           :id,
           :name,
-          label: { text: 'Which department will you work in?', size: 'm' }
+          label: { text: 'Which department will you work in?', size: 's' }
 
         = f.govuk_collection_radio_buttons :welcome_lunch_choice,
           lunch_options,
           :id,
           :name,
-          legend: { text: 'What would you like for lunch on your first day?' }
+          :description,
+          legend: { text: 'What would you like for lunch on your first day?', size: 's' }
       SNIPPET
     end
   end
