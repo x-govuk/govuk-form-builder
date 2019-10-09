72604251f47a63e4e1762d7a4a319157d2ff403b
Peter Yates <peter.yates@graphia.co.uk>
2019-10-09 19:47:25 +0100

    Add specs ensuring conditional content is at root
    The custom matcher is required because using the normal css selectors
    it's impossible to ensure the element we find is at the root of the
    fragment.
    
    Refs #48

---
+++ spec/support/matchers.rb
@@ -0,0 +1,18 @@
+require 'rspec/expectations'
+
+RSpec::Matchers.define :have_root_element_with_class do |class_name|
+  # We're using xpath here because there's no sane way (ie without wrapping
+  # everything in a useless element just for testing) of checking that elements
+  # appear at the root of the fragment
+  #
+  # Note that the ./* matches ANY element `*` at the root `./` of the fragment
+  match do |nokogiri_fragment|
+    !nokogiri_fragment.xpath(%(./*[contains(concat(" ", @class, " "), " #{class_name} ")])).empty?
+  end
+
+  #:nocov:
+  failure_message do |nokogiri_fragment|
+    "expected that #{class_name} would be a class of a root element in fragment: #{nokogiri_fragment}"
+  end
+  #:nocov:
+end
