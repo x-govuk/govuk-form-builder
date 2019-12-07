0dfc865c101fd6305c6c2bcc0b1571b6ced309f6
Peter Yates <peter.yates@graphia.co.uk>
2019-12-07 16:56:51 +0000

    Add specs covering localisation via procs
    The :value_method, :text_method and :hint_method params passed to radio
    collections and the :hint_method params passed to checkbox collections
    now accept procs which can be used to customise the value retrieved from
    the item.
    
    This could be used for simple transformations like upcasing the text or
    more-complex operations like localising the value, as per the provided
    specs
    
    Refs #68

--- spec/support/localisation.rb
+++ spec/support/localisation.rb
@@ -1,7 +1,16 @@
-def with_localisations(localisations, locale: :en)
+DEFAULT_LOCALE = :en
+
+def with_localisations(localisations, locale: DEFAULT_LOCALE)
+  unless locale.eql?(DEFAULT_LOCALE)
+    I18n.available_locales = [DEFAULT_LOCALE, locale]
+    I18n.locale = locale
+  end
   I18n.backend.store_translations(locale, localisations[locale])
 
   yield
 ensure
+  I18n.available_locales = DEFAULT_LOCALE
+  I18n.locale = DEFAULT_LOCALE
+
   I18n.reload!
 end
