18c220ab69dbf60fe225f7c2abb99989e3098804
Peter Yates <peter.yates@graphia.co.uk>
2019-12-14 17:16:21 +0000

    Make the radio field_type check more explicit

--- spec/support/shared/setup_radios.rb
+++ spec/support/shared/setup_radios.rb
@@ -1,5 +1,5 @@
 shared_context 'setup radios' do
-  let(:field_type) { 'input' }
+  let(:field_type) { %s(input[type='radio']) }
   let(:aria_described_by_target) { 'fieldset' }
   let(:attribute) { :favourite_colour }
   let(:label_text) { 'Cherished shade' }
