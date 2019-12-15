4fc28414469597273001a5cae3621c6be4036ee3
Peter Yates <peter.yates@graphia.co.uk>
2019-12-15 12:37:42 +0000

    Add config option for setting default submit text

--- spec/govuk_design_system_formbuilder/builder/configuration_spec.rb
+++ spec/govuk_design_system_formbuilder/builder/configuration_spec.rb
@@ -62,5 +62,34 @@ describe GOVUKDesignSystemFormBuilder::FormBuilder do
         end
       end
     end
+
+    describe 'submit config' do
+      let(:method) { :govuk_submit }
+      let(:args) { [method] }
+      let(:default_submit_button_text) { 'Make it so, number one' }
+
+      subject { builder.send(*args) }
+
+      before do
+        GOVUKDesignSystemFormBuilder.configure do |conf|
+          conf.default_submit_button_text = default_submit_button_text
+        end
+      end
+
+      specify 'should use the default value when no override supplied' do
+        expect(subject).to have_tag('input', with: { type: 'submit', value: default_submit_button_text })
+      end
+
+      context %(overriding with 'Engage') do
+        let(:submit_button_text) { 'Engage' }
+        let(:args) { [method, submit_button_text] }
+
+        subject { builder.send(*args) }
+
+        specify 'should use the default value when no override supplied' do
+          expect(subject).to have_tag('input', with: { type: 'submit', value: submit_button_text })
+        end
+      end
+    end
   end
 end
