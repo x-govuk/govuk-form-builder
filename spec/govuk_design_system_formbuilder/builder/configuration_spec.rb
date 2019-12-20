9ff6072ae1c79e892d184ffcd337055a7f8f93d6
Peter Yates <peter.yates@graphia.co.uk>
2019-12-20 15:11:31 +0000

    Add support for flexible localisation schemas
    The configuration block now supports the following keys for customising
    how the builder's localisation functionality works:
    
    * localisation_schema_fallback
    * localisation_schema_legend
    * localisation_schema_hint
    * localisation_schema_label
    
    The localisation_schema_fallback setting is the only one set by default,
    and it will be used for legends, hints and labels if they are not
    individually configured by their key respectively.
    
    Configuration must be provided as an array; it will be suffixed by the
    object name and attribute name.
    
    The special value `__context__` will be substituted for the contexual
    object; either label, hint or legend. It can be placed anywhere in the
    schema.
    
    Refs #80

--- spec/govuk_design_system_formbuilder/builder/configuration_spec.rb
+++ spec/govuk_design_system_formbuilder/builder/configuration_spec.rb
@@ -163,5 +163,88 @@ describe GOVUKDesignSystemFormBuilder::FormBuilder do
         end
       end
     end
+
+    describe 'localisation configuration' do
+      let(:args) { [method, attribute] }
+      let(:attribute) { :name }
+      let(:method) { :govuk_text_field }
+      subject { builder.send(*args) }
+
+      context 'with a custom fallback localisation schema root' do
+        let(:localisations) { { en: YAML.load_file('spec/support/locales/custom_fallback.en.yaml') } }
+        let(:label_text) { I18n.t(%i(my_shiny_new_app label widgets person name).join('.')) }
+
+        before do
+          GOVUKDesignSystemFormBuilder.configure do |conf|
+            conf.localisation_schema_fallback = %i(my_shiny_new_app __context__ widgets)
+          end
+        end
+
+        specify 'should use the configured localisation fallback schema' do
+          with_localisations(localisations) do
+            expect(subject).to have_tag('label', text: label_text)
+          end
+        end
+      end
+
+      context 'with custom contextual localisation schema roots' do
+        let(:localisations) { { en: YAML.load_file('spec/support/locales/custom_contexts.en.yaml') } }
+
+        context 'labels' do
+          let(:label_text) { I18n.t(%i(a long convoluted schema nested_far_far label too_inconsistently person name).join('.')) }
+
+          before do
+            GOVUKDesignSystemFormBuilder.configure do |conf|
+              conf.localisation_schema_label = %i(a long convoluted schema nested_far_far __context__ too_inconsistently)
+              conf.localisation_schema_fallback = %i(unused)
+            end
+          end
+
+          specify 'should use the configured localisation schema for labels' do
+            with_localisations(localisations) do
+              expect(subject).to have_tag('label', text: label_text)
+            end
+          end
+        end
+
+        context 'hints' do
+          let(:hint_text) { I18n.t(%i(a long convoluted schema hint nested_far_too_deeply person name).join('.')) }
+
+          before do
+            GOVUKDesignSystemFormBuilder.configure do |conf|
+              conf.localisation_schema_hint = %i(a long convoluted schema __context__ nested_far_too_deeply)
+              conf.localisation_schema_fallback = %i(unused)
+            end
+          end
+
+          specify 'should use the configured localisation schema for hints' do
+            with_localisations(localisations) do
+              expect(subject).to have_tag('span', text: hint_text, with: { class: 'govuk-hint' })
+            end
+          end
+        end
+
+        context 'legends' do
+          let(:attribute) { :projects }
+          let(:method) { :govuk_collection_check_boxes }
+          let(:legend_text) { I18n.t(%i(a long convoluted schema legend nested_far_too_deeply person projects).join('.')) }
+
+          subject { builder.send(method, attribute, projects, :id, :name) }
+
+          before do
+            GOVUKDesignSystemFormBuilder.configure do |conf|
+              conf.localisation_schema_legend = %i(a long convoluted schema __context__ nested_far_too_deeply)
+              conf.localisation_schema_fallback = %i(unused)
+            end
+          end
+
+          specify 'should use the configured localisation schema for legends' do
+            with_localisations(localisations) do
+              expect(subject).to have_tag('legend', text: legend_text, with: { class: 'govuk-fieldset__legend' })
+            end
+          end
+        end
+      end
+    end
   end
 end
