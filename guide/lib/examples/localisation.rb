a37bcc15989c61041b3a212916975b5567e6bbb3
Peter Yates <peter.yates@graphia.co.uk>
2019-12-21 12:55:46 +0000

    Add documentation on customising locale schemas
    Refs #80

--- guide/lib/examples/localisation.rb
+++ guide/lib/examples/localisation.rb
@@ -19,5 +19,35 @@ module Examples
         = f.govuk_text_field :favourite_kind_of_hat, label: { size: 'm' }
       SNIPPET
     end
+
+    def role_name
+      <<~SNIPPET
+        = f.govuk_text_field :role, label: { size: 'm' }
+      SNIPPET
+    end
+
+    def custom_locale
+      <<~LOCALE
+        helpers:
+          label:
+            person:
+              role: What role do you play?
+
+        copy:
+          descriptions:
+            hint:
+              subdivision:
+                person:
+                  role: |-
+                    Roles may be achieved or ascribed or they can be accidental
+                    in different situations. An achieved role is a position
+                    that a person assumes voluntarily which reflects personal
+                    skills, abilities, and effort.
+      LOCALE
+    end
+
+    def reset_config
+      GOVUKDesignSystemFormBuilder.reset!
+    end
   end
 end
