389fe7336c939563fc0a3bf3d59278aa5418ced1
jsugarman <joel.sugarman@digital.justice.gov.uk>
2020-01-14 16:46:33 +0000

    Add failing tests

--- spec/govuk_design_system_formbuilder/builder/text_field_spec.rb
+++ spec/govuk_design_system_formbuilder/builder/text_field_spec.rb
@@ -1,9 +1,10 @@
 describe GOVUKDesignSystemFormBuilder::FormBuilder do
   include_context 'setup builder'
 
-  describe('#govuk_text_field')   { it_behaves_like 'a regular input', 'text', 'text' }
-  describe('#govuk_phone_field')  { it_behaves_like 'a regular input', 'phone', 'tel' }
-  describe('#govuk_email_field')  { it_behaves_like 'a regular input', 'email', 'email' }
-  describe('#govuk_url_field')    { it_behaves_like 'a regular input', 'url', 'url' }
-  describe('#govuk_number_field') { it_behaves_like 'a regular input', 'number', 'number' }
+  describe('#govuk_text_field')     { it_behaves_like 'a regular input', 'text', 'text' }
+  describe('#govuk_phone_field')    { it_behaves_like 'a regular input', 'phone', 'tel' }
+  describe('#govuk_email_field')    { it_behaves_like 'a regular input', 'email', 'email' }
+  describe('#govuk_password_field') { it_behaves_like 'a regular input', 'password', 'password' }
+  describe('#govuk_url_field')      { it_behaves_like 'a regular input', 'url', 'url' }
+  describe('#govuk_number_field')   { it_behaves_like 'a regular input', 'number', 'number' }
 end
