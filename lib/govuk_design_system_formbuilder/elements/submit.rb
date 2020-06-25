x-govuk/govuk-form-builder

commit 5772f91280ab6d5205d7f2c74164ba14dbd3bbe6

Simplify HTML building
This is an attempt to make the construction of the HTML output more
obvious. The previous approach worked well for smaller, simpler inputs
but some of the more-complex ones were difficult to read. This was made
more difficult by the multiple layers of indentation for hashes and
longer arrays.

These have now, for the most part, been separated out to their own
methods. The naming convention and structure is now much simpler and
easier to follow.

For consistency, _args methods have been renamed to _options (as they're
the options that configure the element) and what were formerly
extra_args are now kwargs.

lib/govuk_design_system_formbuilder/elements/submit.rb
@@ -18,25 +18,29 @@ module GOVUKDesignSystemFormBuilder
       end
 
       def html
-        safe_join(
-          [
-            @builder.submit(
-              @text,
-              class: %w(button).prefix(brand).push(
-                warning_class,
-                secondary_class,
-                disabled_class,
-                @classes,
-                padding_class(@block_content.present?)
-              ).compact,
-              **extra_args
-            ),
-            @block_content
-          ]
-        )
+        safe_join([submit, @block_content])
       end
 
     private
+
+      def submit
+        @builder.submit(@text, class: submit_classes, **submit_options)
+      end
+
+      def submit_classes
+        %w(button).prefix(brand).push(warning_class, secondary_class, disabled_class, @classes, padding_class(@block_content.present?))
+      end
+
+      def submit_options
+        {
+          formnovalidate: !@validate,
+          disabled: @disabled,
+          data: {
+            module: %(#{brand}-button),
+            'prevent-double-click': @prevent_double_click
+          }.select { |_k, v| v.present? }
+        }
+      end
 
       def warning_class
         %(#{brand}-button--warning) if @warning
@@ -52,16 +56,6 @@ module GOVUKDesignSystemFormBuilder
 
       def disabled_class
         %(#{brand}-button--disabled) if @disabled
-      end
-
-      def extra_args
-        {
-          formnovalidate: !@validate,
-          disabled: @disabled,
-          data: {
-            module: %(#{brand}-button), 'prevent-double-click' => @prevent_double_click
-          }.select { |_k, v| v.present? }
-        }
       end
     end
   end

lib/govuk_design_system_formbuilder/containers/fieldset.rb
@@ -34,10 +34,10 @@ module GOVUKDesignSystemFormBuilder
     private
 
       def legend_content
-        @legend_raw || build_legend
+        @legend_raw || legend
       end
 
-      def build_legend
+      def legend
         if legend_text.present?
           content_tag('legend', class: legend_classes) do
             content_tag(@legend_options.dig(:tag), class: legend_heading_classes) do

lib/govuk_design_system_formbuilder/elements/check_boxes/collection.rb
@@ -23,21 +23,18 @@ module GOVUKDesignSystemFormBuilder
         def html
           Containers::FormGroup.new(@builder, @object_name, @attribute_name).html do
             Containers::Fieldset.new(@builder, @object_name, @attribute_name, legend: @legend, caption: @caption, described_by: [error_id, hint_id, supplemental_id]).html do
-              safe_join(
-                [
-                  supplemental_content.html,
-                  hint_element.html,
-                  error_element.html,
-                  Containers::CheckBoxes.new(@builder, small: @small, classes: @classes).html do
-                    build_collection
-                  end
-                ]
-              )
+              safe_join([supplemental_content.html, hint_element.html, error_element.html, check_boxes])
             end
           end
         end
 
       private
+
+        def check_boxes
+          Containers::CheckBoxes.new(@builder, small: @small, classes: @classes).html do
+            collection
+          end
+        end
 
         # Builds a collection of check {Elements::CheckBoxes::CheckBox}
         # @return [ActiveSupport::SafeBuffer] HTML output
@@ -47,7 +44,7 @@ module GOVUKDesignSystemFormBuilder
         #   be rendered when it happens we need to work on the chance that it might, so
         #   the +link_errors+ variable is set to +true+ if this attribute has errors and
         #   always set back to +false+ after the first checkbox has been rendered
-        def build_collection
+        def collection
           link_errors = has_errors?
 
           @builder.collection_check_boxes(@attribute_name, @collection, @value_method, @text_method) do |check_box|

lib/govuk_design_system_formbuilder/elements/check_boxes/collection_check_box.rb
@@ -10,30 +10,32 @@ module GOVUKDesignSystemFormBuilder
         def initialize(builder, object_name, attribute_name, checkbox, hint_method = nil, link_errors: false)
           super(builder, object_name, attribute_name)
 
-          @checkbox  = checkbox
-          @item      = checkbox.object
-          @value     = checkbox.value
-          @hint_text = retrieve(@item, hint_method)
+          @checkbox    = checkbox
+          @item        = checkbox.object
+          @value       = checkbox.value
+          @hint_text   = retrieve(@item, hint_method)
           @link_errors = link_errors
         end
 
         def html
           content_tag('div', class: %(#{brand}-checkboxes__item)) do
-            safe_join(
-              [
-                @checkbox.check_box(
-                  id: field_id(link_errors: @link_errors),
-                  class: %(#{brand}-checkboxes__input),
-                  aria: { describedby: hint_id }
-                ),
-                label_element.html,
-                hint_element.html
-              ]
-            )
+            safe_join([check_box, label_element.html, hint_element.html])
           end
         end
 
       private
+
+        def check_box
+          @checkbox.check_box(**check_box_options)
+        end
+
+        def check_box_options
+          {
+            id: field_id(link_errors: @link_errors),
+            class: %(#{brand}-checkboxes__input),
+            aria: { describedby: hint_id }
+          }
+        end
 
         def label_element
           @label_element ||= Label.new(@builder, @object_name, @attribute_name, @checkbox, value: @value, link_errors: @link_errors)

lib/govuk_design_system_formbuilder/elements/check_boxes/fieldset_check_box.rb
@@ -24,41 +24,33 @@ module GOVUKDesignSystemFormBuilder
         end
 
         def html
-          safe_join(
-            [
-              content_tag('div', class: %(#{brand}-checkboxes__item)) do
-                safe_join(
-                  [
-                    input,
-                    label_element.html,
-                    hint_element.html,
-                  ]
-                )
-              end,
-              @conditional_content
-            ]
-          )
+          safe_join([check_box, @conditional_content])
         end
 
       private
 
+        def check_box
+          content_tag('div', class: %(#{brand}-checkboxes__item)) do
+            safe_join([input, label_element.html, hint_element.html])
+          end
+        end
+
         def input
-          @builder.check_box(
-            @attribute_name,
-            {
-              id: field_id(link_errors: @link_errors),
-              class: check_box_classes,
-              multiple: @multiple,
-              aria: { describedby: hint_id },
-              data: { 'aria-controls' => @conditional_id }
-            },
-            @value,
-            false
-          )
+          @builder.check_box(@attribute_name, input_options, @value, false)
+        end
+
+        def input_options
+          {
+            id: field_id(link_errors: @link_errors),
+            class: check_box_classes,
+            multiple: @multiple,
+            aria: { describedby: hint_id },
+            data: { 'aria-controls' => @conditional_id }
+          }
         end
 
         def label_element
-          @label_element ||= Elements::Label.new(@builder, @object_name, @attribute_name, checkbox: true, value: @value, link_errors: @link_errors, **label_args)
+          @label_element ||= Elements::Label.new(@builder, @object_name, @attribute_name, checkbox: true, value: @value, link_errors: @link_errors, **label_options)
         end
 
         def hint_element

lib/govuk_design_system_formbuilder/elements/date.rb
@@ -22,21 +22,18 @@ module GOVUKDesignSystemFormBuilder
       def html
         Containers::FormGroup.new(@builder, @object_name, @attribute_name).html do
           Containers::Fieldset.new(@builder, @object_name, @attribute_name, legend: @legend, caption: @caption, described_by: [error_id, hint_id, supplemental_id]).html do
-            safe_join(
-              [
-                supplemental_content.html,
-                hint_element.html,
-                error_element.html,
-                content_tag('div', class: %(#{brand}-date-input)) do
-                  safe_join([day, month, year])
-                end
-              ]
-            )
+            safe_join([supplemental_content.html, hint_element.html, error_element.html, date])
           end
         end
       end
 
     private
+
+      def date
+        content_tag('div', class: %(#{brand}-date-input)) do
+          safe_join([day, month, year])
+        end
+      end
 
       def omit_day?
         @omit_day
@@ -45,18 +42,18 @@ module GOVUKDesignSystemFormBuilder
       def day
         return nil if omit_day?
 
-        date_input_item(:day, link_errors: true)
+        date_part_input(:day, link_errors: true)
       end
 
       def month
-        date_input_item(:month, link_errors: omit_day?)
+        date_part_input(:month, link_errors: omit_day?)
       end
 
       def year
-        date_input_item(:year, width: 4)
+        date_part_input(:year, width: 4)
       end
 
-      def date_input_item(segment, width: 2, link_errors: false)
+      def date_part_input(segment, width: 2, link_errors: false)
         value = @builder.object.try(@attribute_name).try(segment)
 
         content_tag('div', class: %w(date-input__item).prefix(brand)) do
@@ -65,14 +62,14 @@ module GOVUKDesignSystemFormBuilder
               [
                 tag.label(
                   segment.capitalize,
-                  class: date_input_label_classes,
-                  for: date_attribute_id(segment, link_errors)
+                  class: date_part_label_classes,
+                  for: date_part_attribute_id(segment, link_errors)
                 ),
 
                 tag.input(
-                  id: date_attribute_id(segment, link_errors),
-                  class: date_input_classes(width),
-                  name: date_attribute_name(segment),
+                  id: date_part_attribute_id(segment, link_errors),
+                  class: date_part_input_classes(width),
+                  name: date_part_attribute_name(segment),
                   type: 'text',
                   pattern: '[0-9]*',
                   inputmode: 'numeric',
@@ -85,21 +82,21 @@ module GOVUKDesignSystemFormBuilder
         end
       end
 
-      def date_input_classes(width)
+      def date_part_input_classes(width)
         %w(input date-input__input).prefix(brand).tap do |classes|
           classes.push(%(#{brand}-input--width-#{width}))
           classes.push(%(#{brand}-input--error)) if has_errors?
         end
       end
 
-      def date_input_label_classes
+      def date_part_label_classes
         %w(label date-input__label).prefix(brand)
       end
 
       # if the field has errors we want the govuk_error_summary to
       # be able to link to the day field. Otherwise, generate IDs
       # in the normal fashion
-      def date_attribute_id(segment, link_errors)
+      def date_part_attribute_id(segment, link_errors)
         if has_errors? && link_errors
           field_id(link_errors: link_errors)
         else
@@ -107,7 +104,7 @@ module GOVUKDesignSystemFormBuilder
         end
       end
 
-      def date_attribute_name(segment)
+      def date_part_attribute_name(segment)
         format(
           "%<object_name>s[%<attribute_name>s(%<segment>s)]",
           object_name: @object_name,

lib/govuk_design_system_formbuilder/elements/error_message.rb
@@ -13,16 +13,17 @@ module GOVUKDesignSystemFormBuilder
         return nil unless has_errors?
 
         content_tag('span', class: %(#{brand}-error-message), id: error_id) do
-          safe_join(
-            [
-              tag.span('Error: ', class: %(#{brand}-visually-hidden)),
-              message
-            ]
-          )
+          safe_join([error_prefix, error_message])
         end
       end
 
-      def message
+    private
+
+      def error_prefix
+        tag.span('Error: ', class: %(#{brand}-visually-hidden))
+      end
+
+      def error_message
         @builder.object.errors.messages[@attribute_name]&.first
       end
     end

lib/govuk_design_system_formbuilder/elements/error_summary.rb
@@ -12,43 +12,40 @@ module GOVUKDesignSystemFormBuilder
       def html
         return nil unless object_has_errors?
 
-        content_tag('div', class: summary_class, **error_summary_attributes) do
-          safe_join(
-            [
-              tag.h2(@title, id: error_summary_title_id, class: summary_class('title')),
-              content_tag('div', class: summary_class('body')) do
-                content_tag('ul', class: [%(#{brand}-list), summary_class('list')]) do
-                  safe_join(
-                    @builder.object.errors.messages.map do |attribute, messages|
-                      error_list_item(attribute, messages.first)
-                    end
-                  )
-                end
-              end
-            ]
-          )
+        content_tag('div', class: error_summary_class, **error_summary_options) do
+          safe_join([error_title, error_summary])
         end
       end
 
     private
 
-      def error_list_item(attribute, message)
-        content_tag('li') do
-          link_to(
-            message,
-            same_page_link(field_id(attribute)),
-            data: {
-              turbolinks: false
-            }
-          )
+      def error_title
+        tag.h2(@title, id: error_summary_title_id, class: error_summary_class('title'))
+      end
+
+      def error_summary
+        content_tag('div', class: error_summary_class('body')) do
+          content_tag('ul', class: [%(#{brand}-list), error_summary_class('list')]) do
+            safe_join(error_list)
+          end
         end
+      end
+
+      def error_list
+        @builder.object.errors.messages.map do |attribute, messages|
+          error_list_item(attribute, messages.first)
+        end
+      end
+
+      def error_list_item(attribute, message)
+        tag.li(link_to(message, same_page_link(field_id(attribute)), data: { turbolinks: false }))
       end
 
       def same_page_link(target)
         '#'.concat(target)
       end
 
-      def summary_class(part = nil)
+      def error_summary_class(part = nil)
         if part
           %(#{brand}-error-summary).concat('__', part)
         else
@@ -68,7 +65,7 @@ module GOVUKDesignSystemFormBuilder
         @builder.object.errors.any?
       end
 
-      def error_summary_attributes
+      def error_summary_options
         {
           tabindex: -1,
           role: 'alert',

commit 9120767eb8e66b8f2863162309ad889d4164b1c2

Fix failing submit spec
This bug was introduced in 5772f91280ab6d where a compact was
over-zealously removed 😔

Fixes #146

lib/govuk_design_system_formbuilder/elements/submit.rb
@@ -28,7 +28,10 @@ module GOVUKDesignSystemFormBuilder
       end
 
       def submit_classes
-        %w(button).prefix(brand).push(warning_class, secondary_class, disabled_class, @classes, padding_class(@block_content.present?))
+        %w(button)
+          .prefix(brand)
+          .push(warning_class, secondary_class, disabled_class, @classes, padding_class(@block_content.present?))
+          .compact
       end
 
       def submit_options