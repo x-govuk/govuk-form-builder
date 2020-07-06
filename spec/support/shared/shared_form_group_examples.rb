66c831367ed77f289d31e3fdb057230036c8d14e
Peter Yates <peter.yates@graphia.co.uk>
2020-07-06 19:10:36 +0100

    Always pass a block into spec subject
    This allows the same example group to be used across all helpers
    (including those that are primarily containers) as some (the ones that
    end in _fieldset) will fail unless a block is present
    
    Refs #158

--- spec/support/shared/shared_form_group_examples.rb
+++ spec/support/shared/shared_form_group_examples.rb
@@ -1,7 +1,11 @@
 shared_examples 'a field that contains a customisable form group' do
   example_group 'when custom classes are provided' do
     let(:default_class) { %w(govuk-form-group) }
-    subject { builder.send(*args, form_group_classes: custom_classes) }
+    let(:block_content) { %(Are you acquainted with our state's stringent usury laws?) }
+
+    subject do
+      builder.send(*args, form_group_classes: custom_classes) { builder.tag.span(block_content) }
+    end
 
     context 'classes passed in as an array' do
       let(:custom_classes) { %w(red speckled) }
