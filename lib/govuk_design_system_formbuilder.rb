x-govuk/govuk-form-builder

commit 08fe4ffc0528ff89cab2bda1d4ec3f60f26ae5ae

Favour tag over content_tag, remove capture blocks
Using #content_tag doesn't really make sense when the tag is fixed, so
favour using #tag instead.

Also, as everything goes through #safe_join, #capture is unnecessary,
remove it and remove its delegation.

lib/govuk_design_system_formbuilder.rb
@@ -88,7 +88,7 @@ module GOVUKDesignSystemFormBuilder
   end
 
   class FormBuilder < ActionView::Helpers::FormBuilder
-    delegate :content_tag, :tag, :safe_join, :safe_concat, :capture, :link_to, :raw, to: :@template
+    delegate :content_tag, :tag, :safe_join, :link_to, :raw, to: :@template
 
     include GOVUKDesignSystemFormBuilder::Builder
   end

lib/govuk_design_system_formbuilder/base.rb
@@ -8,14 +8,14 @@ module GOVUKDesignSystemFormBuilder
   end
 
   class Base
-    delegate :capture, :content_tag, :safe_join, :tag, :raw, :link_to, to: :@builder
+    delegate :content_tag, :safe_join, :tag, :raw, :link_to, to: :@builder
     delegate :config, to: GOVUKDesignSystemFormBuilder
 
     def initialize(builder, object_name, attribute_name, &block)
       @builder        = builder
       @object_name    = object_name
       @attribute_name = attribute_name
-      @block_content  = capture { block.call } if block_given?
+      @block_content  = block.call if block_given?
     end
 
     # objects that implement #to_s can be passed directly into #safe_join

lib/govuk_design_system_formbuilder/containers/check_boxes_fieldset.rb
@@ -13,7 +13,7 @@ module GOVUKDesignSystemFormBuilder
         @small              = small
         @classes            = classes
         @form_group_classes = form_group_classes
-        @block_content      = capture { block.call }
+        @block_content      = block.call
       end
 
       def html

lib/govuk_design_system_formbuilder/containers/radio_buttons_fieldset.rb
@@ -14,7 +14,7 @@ module GOVUKDesignSystemFormBuilder
         @hint_text          = hint_text
         @classes            = classes
         @form_group_classes = form_group_classes
-        @block_content      = capture { block.call }
+        @block_content      = block.call
       end
 
       def html

lib/govuk_design_system_formbuilder/elements/submit.rb
@@ -14,7 +14,7 @@ module GOVUKDesignSystemFormBuilder
         @classes              = classes
         @validate             = validate
         @disabled             = disabled
-        @block_content        = capture { block.call } if block_given?
+        @block_content        = block.call if block_given?
       end
 
       def html

lib/govuk_design_system_formbuilder/traits/conditional.rb
@@ -8,9 +8,7 @@ module GOVUKDesignSystemFormBuilder
       end
 
       def wrap_conditional(block)
-        content_tag('div', class: conditional_classes, id: conditional_id) do
-          capture { block.call }
-        end
+        tag.div(class: conditional_classes, id: conditional_id) { block.call }
       end
     end
   end

lib/govuk_design_system_formbuilder/containers/character_count.rb
@@ -14,7 +14,7 @@ module GOVUKDesignSystemFormBuilder
       def html
         return yield unless limit?
 
-        content_tag('div', **options) { yield }
+        tag.div(**options) { yield }
       end
 
     private

lib/govuk_design_system_formbuilder/containers/check_boxes.rb
@@ -8,7 +8,7 @@ module GOVUKDesignSystemFormBuilder
       end
 
       def html
-        content_tag('div', **options) { yield }
+        tag.div(**options) { yield }
       end
 
     private

commit 84092cf9605927ec241b8fc985f00d2ebb912514

Wrap the calling of blocks in capture
This bug was introduced in 08fe4ffc05 and wasn't picked up in the tests,
probably due to the fact we're not using a templating layer here just
plain Ruby. It turns out capture is needed to prevent the block content
from appearing once where it's meant to and sometimes immediately when
the block is called.

lib/govuk_design_system_formbuilder.rb
@@ -88,7 +88,7 @@ module GOVUKDesignSystemFormBuilder
   end
 
   class FormBuilder < ActionView::Helpers::FormBuilder
-    delegate :content_tag, :tag, :safe_join, :link_to, to: :@template
+    delegate :content_tag, :tag, :safe_join, :link_to, :capture, to: :@template
 
     include GOVUKDesignSystemFormBuilder::Builder
   end

lib/govuk_design_system_formbuilder/base.rb
@@ -8,14 +8,14 @@ module GOVUKDesignSystemFormBuilder
   end
 
   class Base
-    delegate :content_tag, :safe_join, :tag, :link_to, to: :@builder
+    delegate :content_tag, :safe_join, :tag, :link_to, :capture, to: :@builder
     delegate :config, to: GOVUKDesignSystemFormBuilder
 
     def initialize(builder, object_name, attribute_name, &block)
       @builder        = builder
       @object_name    = object_name
       @attribute_name = attribute_name
-      @block_content  = block.call if block_given?
+      @block_content  = capture { block.call } if block_given?
     end
 
     # objects that implement #to_s can be passed directly into #safe_join

lib/govuk_design_system_formbuilder/containers/check_boxes_fieldset.rb
@@ -13,7 +13,7 @@ module GOVUKDesignSystemFormBuilder
         @small              = small
         @classes            = classes
         @form_group_classes = form_group_classes
-        @block_content      = block.call
+        @block_content      = capture { block.call }
       end
 
       def html

lib/govuk_design_system_formbuilder/containers/radio_buttons_fieldset.rb
@@ -14,7 +14,7 @@ module GOVUKDesignSystemFormBuilder
         @hint_text          = hint_text
         @classes            = classes
         @form_group_classes = form_group_classes
-        @block_content      = block.call
+        @block_content      = capture { block.call }
       end
 
       def html

lib/govuk_design_system_formbuilder/elements/submit.rb
@@ -14,7 +14,7 @@ module GOVUKDesignSystemFormBuilder
         @classes              = classes
         @validate             = validate
         @disabled             = disabled
-        @block_content        = block.call if block_given?
+        @block_content        = capture { block.call } if block_given?
       end
 
       def html

lib/govuk_design_system_formbuilder/traits/conditional.rb
@@ -8,7 +8,9 @@ module GOVUKDesignSystemFormBuilder
       end
 
       def wrap_conditional(block)
-        tag.div(class: conditional_classes, id: conditional_id) { block.call }
+        tag.div(class: conditional_classes, id: conditional_id) do
+          capture { block.call }
+        end
       end
     end
   end