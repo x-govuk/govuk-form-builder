45e7566d8e11c6e00e65f404862457360047ae65
Peter Yates <peter.yates@graphia.co.uk>
2020-10-27 16:39:02 +0000

    Add specs for the localising fieldset contents

--- spec/support/shared/shared_localisation_examples.rb
+++ spec/support/shared/shared_localisation_examples.rb
@@ -251,3 +251,17 @@ shared_examples 'a field that supports localised collection hints' do
     end
   end
 end
+
+shared_examples 'a field that supports localised fieldset hints' do
+  let(:localisations) { { en: YAML.load_file('spec/support/locales/collection_hints.en.yaml').deep_symbolize_keys } }
+  let(:expected_hints) { localisations.dig(:en, :helpers, :hint, :person, localisation_key) }
+
+  specify 'the hints should be set by localisation' do
+    with_localisations(localisations) do
+      expected_hints.each do |id, hint|
+        expected_id = ["person", field_name_selector, id, "hint"].join("-")
+        expect(subject).to have_tag("span", with: { id: expected_id }, text: hint)
+      end
+    end
+  end
+end
