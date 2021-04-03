92ed0402a41a3f37a03a216f5f7148e6c00cd44a
Peter Yates <peter.yates@graphia.co.uk>
2021-04-03 12:59:13 +0100

    Add a specialised attribute parser to help merging
    This class, GOVUKDesignSystemFormBuilder::Traits::HTMLAttributes::Parser
    has the responsibility of transforming the values of HTML attributes that
    are lists represented by strings (e.g., class) into arrays.
    
    It's currently a naïve implementation that pays no attention to the
    TARGETS constant which will eventually govern which attributes it
    modifies.

--- lib/govuk_design_system_formbuilder/traits/html_attributes.rb
+++ lib/govuk_design_system_formbuilder/traits/html_attributes.rb
@@ -1,8 +1,45 @@
 module GOVUKDesignSystemFormBuilder
   module Traits
     module HTMLAttributes
+      class Parser
+        # target any element where the content is a string that represents a
+        # list of things separated by a space, e.g.,
+        #
+        # <span class="red spots">xyz</span>
+        #
+        # could be respresented as
+        #
+        # = span(class: %w(red spots)) { "xyz" }
+        TARGETS = [%i(class), %i(aria describedby)].freeze
+
+        def initialize(attributes)
+          @attributes = parse(attributes)
+        end
+
+        def to_h
+          @attributes
+        end
+
+      private
+
+        def parse(attributes)
+          attributes.tap do |a|
+            a[:class]              = a[:class].split              if has_class_string?(a)
+            a[:aria][:describedby] = a[:aria][:describedby].split if has_aria_describedby_string?(attributes)
+          end
+        end
+
+        def has_class_string?(attributes)
+          attributes.key?(:class) && attributes[:class].is_a?(String)
+        end
+
+        def has_aria_describedby_string?(attributes)
+          attributes.dig(:aria, :describedby)&.is_a?(String)
+        end
+      end
+
       def attributes(html_attributes = {})
-        options.deeper_merge(html_attributes)
+        options.deeper_merge(Parser.new(html_attributes).to_h)
       end
     end
   end
--- spec/support/shared/shared_html_attribute_examples.rb
+++ spec/support/shared/shared_html_attribute_examples.rb
@@ -1,51 +1,97 @@
 shared_examples 'a field that allows extra HTML attributes to be set' do
-  custom_attributes = {
-    required:     { provided: true,              expected: 'required' },
-    autocomplete: { provided: false,             expected: 'false' },
-    placeholder:  { provided: 'Seymour Skinner', expected: 'Seymour Skinner' },
-    data:         { provided: { a: 'b' },        expected: { 'data-a' => 'b' } },
-    aria:         { provided: { c: 'd' },        expected: { 'aria-c' => 'd' } },
-    class:        { provided: %w(red spots),     expected: %w(red spots) }
-  }
-
-  let(:custom_attributes) { custom_attributes }
-
   let(:example_block) { proc { builder.tag.span('block text') } }
-  special_keys = %i(data aria)
 
-  subject do
-    builder.send(*args, **extract_args(custom_attributes, :provided), &example_block)
-  end
+  context 'with args in the expected formats' do
+    custom_attributes = {
+      required:     { provided: true,              expected: 'required' },
+      autocomplete: { provided: false,             expected: 'false' },
+      placeholder:  { provided: 'Seymour Skinner', expected: 'Seymour Skinner' },
+      data:         { provided: { a: 'b' },        expected: { 'data-a' => 'b' } },
+      aria:         { provided: { c: 'd' },        expected: { 'aria-c' => 'd' } },
+      class:        { provided: %w(red spots),     expected: %w(red spots) }
+    }
 
-  describe 'input tag should have the extra attributes:' do
-    extract_args(custom_attributes, :expected).each do |key, val|
-      case
+    let(:custom_attributes) { custom_attributes }
 
-      # class is dealt with as a special case because we want to ensure that whatever
-      # extra classes we provide are *added* to the pre-existing one (expected_class)
-      # instead of replacing it
-      when key == :class
-        specify "#{key} has classes #{val}" do
-          expect(subject).to have_tag(described_element, with: { class: val.concat(Array.wrap(expected_class)) })
-        end
+    special_keys = %i(data aria)
 
-      # we need to deal with the _special keys_, data and aria, separately
-      # because Rails accepts them via a nested hash and automatically expands
-      # them out; e.g,
-      # tag.element(data: { a: 'b' }) will result in <element data-a="b" ...>
-      when key.in?(special_keys)
-        specify "#{val} is set" do
-          val.each do |data_attribute_name, data_attribute_value|
-            expect(subject).to have_tag(described_element, with: { data_attribute_name => data_attribute_value })
+    subject do
+      builder.send(*args, **extract_args(custom_attributes, :provided), &example_block)
+    end
+
+    describe 'input tag should have the extra attributes:' do
+      extract_args(custom_attributes, :expected).each do |key, val|
+        case
+
+        # class is dealt with as a special case because we want to ensure that whatever
+        # extra classes we provide are *added* to the pre-existing one (expected_class)
+        # instead of replacing it
+        when key == :class
+          specify "#{key} has classes #{val}" do
+            expect(subject).to have_tag(described_element, with: { class: val.concat(Array.wrap(expected_class)) })
+          end
+
+        # we need to deal with the _special keys_, data and aria, separately
+        # because Rails accepts them via a nested hash and automatically expands
+        # them out; e.g,
+        # tag.element(data: { a: 'b' }) will result in <element data-a="b" ...>
+        when key.in?(special_keys)
+          specify "#{val} is set" do
+            val.each do |data_attribute_name, data_attribute_value|
+              expect(subject).to have_tag(described_element, with: { data_attribute_name => data_attribute_value })
+            end
+          end
+
+        # all others, just check that the attribute has been set properly
+        else
+          specify "#{key} is #{val}" do
+            expect(subject).to have_tag(described_element, with: { key => val })
           end
         end
-
-      # all others, just check that the attribute has been set properly
-      else
-        specify "#{key} is #{val}" do
-          expect(subject).to have_tag(described_element, with: { key => val })
-        end
       end
+    end
+  end
+
+  context 'when classes are provided as a string' do
+    let(:custom_classes) { "purple waves" }
+    let(:combined_classes) { custom_classes.split.append(expected_class) }
+
+    subject do
+      builder.send(*args, class: custom_classes, &example_block)
+    end
+
+    specify 'the extra and default classes should both be present' do
+      expect(parsed_subject).to contain_element(described_element).with_attribute('class').that_has_values(*combined_classes)
+    end
+  end
+end
+
+shared_examples 'a field that allow nested HTML attributes to be set' do
+  let(:example_block) { proc { builder.tag.span('block text') } }
+
+  context 'when aria-describedby is provided as a string' do
+    let(:custom_aria_described_by) { "a-descriptive-paragraph-id" }
+    let(:hint_args) { { hint: { text: "ignore this" } } }
+
+    let(:generated_hint_id) do
+      return nil if attribute.blank?
+
+      [
+        object_name,
+        attribute,
+        (defined?(value) && value),
+        'hint'
+      ].compact.map { |e| underscores_to_dashes(e) }.join("-")
+    end
+
+    let(:combined_aria_described_by) { custom_aria_described_by.split.append(generated_hint_id) }
+
+    subject do
+      builder.send(*args, aria: { describedby: custom_aria_described_by }, **hint_args, &example_block)
+    end
+
+    specify 'the aria-describedby should both be present' do
+      expect(parsed_subject).to contain_element(described_element).with_attribute("aria-describedby").that_has_values(custom_aria_described_by, generated_hint_id)
     end
   end
 end
