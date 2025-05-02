7d48b9f5837d7abd4d1f3f2a05432f4101aa74e0
Peter Yates <peter.yates@graphia.co.uk>
2025-05-02 21:13:56 +0100

    Fix more MDN links

--- guide/lib/helpers/related_info.rb
+++ guide/lib/helpers/related_info.rb
@@ -14,7 +14,7 @@ module Helpers
         'Form builder documentation for checkbox fieldsets' => 'https://www.rubydoc.info/gems/govuk_design_system_formbuilder/GOVUKDesignSystemFormBuilder/Builder#govuk_check_boxes_fieldset-instance_method',
         'Form builder documentation for individual checkboxes' => 'https://www.rubydoc.info/gems/govuk_design_system_formbuilder/GOVUKDesignSystemFormBuilder/Builder#govuk_check_box-instance_method',
         'GOV.UK design system checkbox component' => 'https://design-system.service.gov.uk/components/checkboxes/',
-        'MDN checkbox documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox'
+        'MDN checkbox documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/input/checkbox'
       }
     end
 
@@ -29,7 +29,7 @@ module Helpers
       {
         'Form builder documentation for select boxes' => 'https://www.rubydoc.info/gems/govuk_design_system_formbuilder/GOVUKDesignSystemFormBuilder/Builder#govuk_collection_select-instance_method',
         'GOV.UK design system select component' => 'https://design-system.service.gov.uk/components/select/',
-        'MDN select element documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select'
+        'MDN select element documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/select'
       }
     end
 
@@ -37,7 +37,7 @@ module Helpers
       {
         'Form builder documentation for submit buttons' => 'https://www.rubydoc.info/gems/govuk_design_system_formbuilder/GOVUKDesignSystemFormBuilder/Builder#govuk_submit-instance_method',
         'GOV.UK design system submit component' => 'https://design-system.service.gov.uk/components/button/',
-        'MDN submit element documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/submit'
+        'MDN submit element documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/input/submit'
       }
     end
 
@@ -47,7 +47,7 @@ module Helpers
         'Form builder documentation for radio button fieldsets' => 'https://www.rubydoc.info/gems/govuk_design_system_formbuilder/GOVUKDesignSystemFormBuilder/Builder#govuk_radio_buttons_fieldset-instance_method',
         'Form builder documentation for individual radio buttons' => 'https://www.rubydoc.info/gems/govuk_design_system_formbuilder/GOVUKDesignSystemFormBuilder/Builder#govuk_radio_button-instance_method',
         'GOV.UK design system radios component' => 'https://design-system.service.gov.uk/components/radios/',
-        'MDN radio button documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio'
+        'MDN radio button documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/input/radio'
       }
     end
 
@@ -68,7 +68,7 @@ module Helpers
         'Form builder documentation for URL fields' => 'https://www.rubydoc.info/gems/govuk_design_system_formbuilder/GOVUKDesignSystemFormBuilder/Builder#govuk_url_field-instance_method',
 
         'GOV.UK design system text input' => 'https://design-system.service.gov.uk/components/text-input/',
-        'MDN input mode documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Global_attributes/inputmode',
+        'MDN input mode documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Global_attributes/inputmode',
         'WhatWG documentation on input the type attribute' => 'https://html.spec.whatwg.org/multipage/input.html#states-of-the-type-attribute'
       }
     end
@@ -77,7 +77,7 @@ module Helpers
       {
         'Form builder documentation for text areas' => 'https://www.rubydoc.info/gems/govuk_design_system_formbuilder/GOVUKDesignSystemFormBuilder/Builder#govuk_text_area-instance_method',
         'GOV.UK design system text area component' => 'https://design-system.service.gov.uk/components/textarea/',
-        'MDN text area documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/textarea'
+        'MDN text area documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/textarea'
       }
     end
 
@@ -85,7 +85,7 @@ module Helpers
       {
         'Form builder documentation for file upload fields' => 'https://www.rubydoc.info/gems/govuk_design_system_formbuilder/GOVUKDesignSystemFormBuilder/Builder#govuk_file_field-instance_method',
         'GOV.UK design system file upload component' => 'https://design-system.service.gov.uk/components/file-upload/',
-        'MDN file upload documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file'
+        'MDN file upload documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/input/file'
       }
     end
 
@@ -93,7 +93,7 @@ module Helpers
       {
         'Form builder documentation for password inputs' => 'https://www.rubydoc.info/gems/govuk_design_system_formbuilder/GOVUKDesignSystemFormBuilder/Builder#govuk_password_field-instance_method',
         'GOV.UK design system radios component' => 'https://design-system.service.gov.uk/components/password-input/',
-        'MDN radio button documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/password'
+        'MDN radio button documentation' => 'https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/input/password'
       }
     end
   end
