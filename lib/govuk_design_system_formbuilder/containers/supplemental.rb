a5ca9101058cc8d01abe8ab77b428bb87760a152
Peter Yates <peter.yates@graphia.co.uk>
2025-09-19 23:18:07 +0100

    Remove link to supplemental content
    This change removes the aria-describedby link between labels/legends and
    supplemental content, and the warnings that were added in #456.
    
    It still allows arbitrary HTML content to be passed in, and it'll be
    rendered in the same place - but importantly, it won't be read out by
    screen readers. This can be a frustrating experience for screenr eader
    users if there's multiple paragraphs read out between the label and the
    input itself.
    
    Fixes #445.

--- lib/govuk_design_system_formbuilder/containers/supplemental.rb
+++ lib/govuk_design_system_formbuilder/containers/supplemental.rb
@@ -10,15 +10,7 @@ module GOVUKDesignSystemFormBuilder
       def html
         return if @content.blank?
 
-        warn("Supplemental content is deprecated and support will soon be removed. See https://github.com/x-govuk/govuk-form-builder/issues/445")
-
-        tag.div(id: supplemental_id) { @content }
-      end
-
-    private
-
-      def supplemental_id
-        build_id('supplemental')
+        tag.div { @content }
       end
     end
   end
