9e486b0ef4f97407624839522f960f57bb49e831
Stephen Nelson <stephen@katalyst.com.au>
2026-06-11 16:14:58 +0930

    Fix #627: Merge password form group options

--- lib/govuk_design_system_formbuilder/elements/password.rb
+++ lib/govuk_design_system_formbuilder/elements/password.rb
@@ -2,6 +2,7 @@ module GOVUKDesignSystemFormBuilder
   module Elements
     class Password < Base
       using PrefixableArray
+      using HTMLAttributesUtils
 
       include Traits::Error
       include Traits::Hint
@@ -60,7 +61,7 @@ module GOVUKDesignSystemFormBuilder
           **i18n_data,
           class: %(#{brand}-password-input),
           data: { module: %(#{brand}-password-input) },
-        }
+        }.deep_merge_html_attributes(@form_group)
       end
 
       def password_input
